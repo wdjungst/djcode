2a530424cd80efeac74b6adbdc237c5c755397ec
David Jungst <djungst@gmail.com>
2018-08-24 10:13:40 -0600

    some style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,43 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from 'react'
 import styled, { keyframes } from 'styled-components'
+import { Divider } from 'semantic-ui-react'
 import { Flex } from './styles'
 
+const colorChange = keyframes`
+  from {
+    color: black;
+  }
+
+  to {
+    color: lightblue; 
+  }
+`
+
 const BigLetter = styled.span`
-  font-size: 9rem;
+  font-size: ${ props => props.size || '9rem' };
+  font-face: ${ props => props.fontFace ? props.fontFace : '' };
   color: white;
 `
 
+const CodeSection = styled.div`
+`
+
 const Container = styled.div`
-  display: flex;
-  height: 50vh;
-  justify-content: center;
-  align-items: center;
+  padding-top: 50px;
+  height: 70vh;
   background-color: #000;
 `
 
 const Canvas = styled.canvas`
-  width: ${ props => props.expanded ? '250px' : '0px' };
+  width: ${ props => props.expanded ? '250px' : '1px' };
   transition: width 1s linear;
+`
+
+const SpinBox = styled.div`
+  width: 250px;
+  font-size: 6rem;
+  animation: ${colorChange} 4s linear infinite;
+  animation-delay: 3.8s;
 `
 
 class Name extends Component {
@@ -64,28 +84,63 @@ class Home extends Component {
   state = { 
     expanded: false,
     start: false,
+    words: [
+      'speak',
+      'write',
+      'teach',
+      'learn',
+    ],
+    word: ''
   }
 
   componentDidMount() {
     setTimeout( () => this.setState({ expanded: true }), 1000 )
     setTimeout( () => this.setState({ start: true }), 2000 )
+    this.interval = setInterval( this.changeWord, 4000 )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  changeWord = () => {
+    const { words, word } = this.state
+    let index = words.findIndex( w => w === word)
+    if (index >= 0) {
+      if (index === words.length - 1)
+        index = 0
+      else
+        index++
+    } else {
+     index = 0 
+    }
+
+    this.setState({ word: words[index] })
   }
 
   render() {
-    const { expanded, start } = this.state
+    const { expanded, start, word } = this.state
 
     return (
       <Container>
-        <BigLetter className="name-animate">D</BigLetter>
-        <Canvas className="name-animate" expanded={expanded} id="avid" height="150"></Canvas>
-        <BigLetter className="name-animate">J</BigLetter>
-        <Canvas className="name-animate" expanded={expanded} id="ungst" height="150"></Canvas>
-        { start && 
-          <Fragment>
-            <Name fontText="avid" />
-            <Name fontText="ungst" />
-          </Fragment>
-        }
+        <Flex height="50%" justifyContent="center" alignItems="center" >
+          <BigLetter className="name-animate">D</BigLetter>
+          <Canvas className="name-animate" expanded={expanded} id="avid" height="150"></Canvas>
+          <BigLetter className="name-animate">J</BigLetter>
+          <Canvas className="name-animate" expanded={expanded} id="ungst" height="150"></Canvas>
+          { start && 
+            <Fragment>
+              <Name fontText="avid" />
+              <Name fontText="ungst" />
+            </Fragment>
+          }
+        </Flex>
+        <CodeSection>
+          <Flex justifyContent="center">
+            <SpinBox>{word}</SpinBox>
+            <BigLetter fontFace="Roboto" size="6.5rem">CODE</BigLetter>
+          </Flex>
+        </CodeSection>
       </Container>
     );
   }
--- client/src/components/styles.js
+++ client/src/components/styles.js
@@ -2,4 +2,12 @@ import styled from 'styled-components'
 
 export const Flex =  styled.div`
   display: flex;
+  justify-content: ${ props => props.justifyContent };
+  align-items: ${ props => props.alignItems };
+  height: ${ props => props.height };
+`
+
+export const FlexNum = styled.div`
+  flex: ${ props => props.num };
+  justify
 `
