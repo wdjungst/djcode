0c053da6464c9ba51ac87bb04d91625f8c77646f
David Jungst <djungst@gmail.com>
2018-10-25 15:47:36 -0600

    remove protected route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,6 @@ import Login from './Login';
 import Register from './Register';
 import Flash from './Flash';
 import Home from './Home';
-import ProtectedRoute from './ProtectedRoute';
 import AuthRoute from './AuthRoute';
 import FetchUser from './FetchUser';
 
